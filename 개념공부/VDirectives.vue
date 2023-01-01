d0d91eff6f7abbb01b36b2f6bcf4ad17774a5b9c
Hong-sk <sooyun9600@naver.com>
2023-01-01 23:50:03 +0900

    v-cloak

--- 개념공부/VDirectives.vue
+++ 개념공부/VDirectives.vue
@@ -8,6 +8,11 @@
 	<h3>v-memo 사용법</h3>
 	<div v-memo="[exampleNum]">{{ exampleNum }}</div>
 	<button @click="() => (exampleNum = exampleNum++)">증가</button>
+
+	<hr />
+	<div>
+		<div class="loading">v-cloak는 로딩 등의 특수 상황에서 사용합니다.</div>
+	</div>
 </template>
 
 <script>
